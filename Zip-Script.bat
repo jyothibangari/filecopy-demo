b08f778f78f6718983427c8194b63b537c89e441
jyothibangari <110468120+jyothibangari@users.noreply.github.com>
2022-08-30 12:42:59 +0530

    Update Zip-Script.bat

--- Zip-Script.bat
+++ Zip-Script.bat
@@ -5,4 +5,4 @@ SET Source=E:\Prodigy\tasks\Copy-Scripts\Source\file*
 SET Desination=E:\Prodigy\tasks\pipeline_tocopyfiles_using_bat\test1.zip
 SET zipUtility="C:\Program Files\7-Zip\7z.exe"
 
-%zipUtility a "%Desination%" "%Source%"
+%zipUtility% a "%Desination%" "%Source%"
